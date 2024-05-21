c98ce14b587523b66dcc39877ead4a5c751435db
benjamin-olivera <benjamin.oliveracolonio@gmail.com>
2024-05-21 18:35:42 -0500

    adding while not

--- black_jack.py
+++ black_jack.py
@@ -7,9 +7,11 @@ def deal_card():
   return card  
 user_cards = []
 computer_cards = []
+is_game_over = False
 for _ in range(2):
   user_cards.append(deal_card())
   computer_cards.append(deal_card())
+
 def calculate_score(cards):
   if sum(cards) == 21 and len(cards) == 2:
     return 0
@@ -17,3 +19,19 @@ def calculate_score(cards):
     cards.remove(11)
     cards.append(1)
   return sum(cards)
+while not is_game_over:
+    user_score = calculate_score(user_cards)
+    computer_score = calculate_score(computer_cards)
+    print(f" Your cards: {user_cards}, current score: {user_score}")
+    print(f" Computer's first card: {computer_cards[0]}")
+    if user_score == 0 or computer_score == 0 or user_score > 21:
+      is_game_over = True
+    else:
+      user_should_deal =  input("Type 'y' to get another card, type 'n' to pass: ")
+    if user_should_deal == 'y':
+      user_cards.append(deal_card())
+while computer_score != 0 and computer_score < 17:
+  computer_cards.append(deal_card())
+  computer_score = calculate_score(computer_cards)
+  
+ 
